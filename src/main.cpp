9af54173c2eb614585c12a620330cfe1c32110f9
Charlie-Porto <charlie@d20.solutions>
2022-05-27 12:17:12 -0500

    added initial code for SDL2 project

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "constants/static_variables.cpp"
+#include "SDL_core/simulation_manager.cpp"
+
+#include "ecs/ControlPanel.cpp"
+
+/* components */
+
+/* systems */
+
+// make sure to add framerate timer to include folder
+#include <simple_framerate_timer.cpp>
+
+
+/*---------- screen dimensions ----------*/
+const int screenx = global_const::screen_x;
+const int screeny = global_const::screen_y;
+/*---------------------------------------*/
+ControlPanel control;
+int main(int argc, const char * argv[]) {
+    /*------------------------- SDL setup ----------------------------*/ 
+    /* set up time and frames */
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;    
+    int frameStart;
+    int frameTime;
+    
+    /* create simulation class instance */ 
+    Simulation* simulation = new Simulation;
+    simulation->init("PineconeEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenx, screeny, false);
+    std::cout << "SDL Simulation successfully initialized." << std::endl;
+
+    /* set randomness seed */
+    // set_srand();
+
+    /* Ordinary Setup items */
+    // TODO: move control to separate file? 
+    control.Init();
+
+    /* Register Components */
+
+
+    /* Register Systems */
+
+    
+    /* Create Entities */
+    
+
+
+
+    simple_framerate_timer simple_timer = simple_framerate_timer();
+
+    /*----------Main Loop---------------------*/
+    while (simulation->running()) {
+        
+        /* SDL events and updating */ 
+        int frameStart = SDL_GetTicks();       
+        simulation->handleEvents();
+        simulation->update();
+        simulation->clearRenderer();
+
+        double t = frameStart/1000.0;
+
+        // update timer 
+        int time = int(t);
+        simple_timer.update_timer(time);
+
+
+        /*~~~~~~~~~------------- Do Stuff and Update ----------------*/
+        double ticks = (SDL_GetTicks()/1000.0);
+
+
+        /*~~~~~~~~~-------------- Draw and Render --------------------*/
+        // draw_system->UpdateEntities();
+        simulation->render();
+
+        /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+        /* Clear events list */
+        simulation->clear_frame_events();
+
+        /* check how much time has passed during frame */
+        frameTime = SDL_GetTicks() - frameStart;
+
+        /* if frame too fast, delay the frame */
+        if ( frameDelay > frameTime )
+        {
+            SDL_Delay( frameDelay - frameTime );
+        }
+    }
+
+    simulation->clean();
+
+    delete simulation;
+    
+    return 0;
+}
+
